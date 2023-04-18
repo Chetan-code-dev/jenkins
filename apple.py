bc4b1eb07fca8397c112436f9f902090b2db111e
Chetan-code-dev <129928740+Chetan-code-dev@users.noreply.github.com>
2023-04-18 10:35:45 +0530

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,9 +1,18 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+from flask import Flask, render_template, request
 
-# Print the first 10 numbers in the Fibonacci sequence
-for i in range(10):
-    print(fibonacci(i))
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # Do something with the form data, like sending an email or saving to a database
+    return render_template('submit.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
